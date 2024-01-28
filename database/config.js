266ee0ad349dd487df04031c13bc278ee91161c9
Leandro Martini <leandromartini86@gmail.com>
2024-01-28 16:40:48 -0300

    siguiente prueba

--- database/config.js
+++ database/config.js
@@ -3,8 +3,7 @@ const mongoose = require("mongoose");
 const dbConnection = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_CNN, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
+     
     });
 
     console.log("Base de datos online");
